1a1b8814ec6ae0ac223effd5930c1d4158170032
SuHyun Lee <101233934+CreatePath@users.noreply.github.com>
2022-11-11 16:07:17 +0900

    refactored code final

--- models/users.js
+++ models/users.js
@@ -1,51 +1,48 @@
-const db = require('../config/db/db');
 const { execute } = require('../utils/dbQuery');
 
 // DB에 쿼리 보내기 
 module.exports = {
-    user : {
-        insert_user : async (user_email, user_pw, user_nickname, salt) => {
-          	let queryString = `insert into users(user_email, user_pw, user_nickname, salt, point) 
-			value ("${user_email}" , "${user_pw}" , "${user_nickname}", "${salt}", 0)`; 
-            execute(queryString);
-        },
-        
-        get_user_by_email : async (user_email) => {
-    		let queryString = `select * from users where user_email = "${user_email}"`;
-            result = await execute(queryString);
-            return result[0][0];
-		},
-        
-        get_user_by_nickname : async (user_nickname) => {
-            let queryString = `select * from users where user_nickname = "${user_nickname}"`;
-            result = await execute(queryString);
-            return result[0][0];
-        },
+    insert_user : async (user_email, user_pw, user_nickname, salt) => {
+        let queryString = `insert into users(user_email, user_pw, user_nickname, salt, point) 
+        value ("${user_email}" , "${user_pw}" , "${user_nickname}", "${salt}", 0)`; 
+        return execute(queryString);
+    },
+    
+    get_user_by_email : async (user_email) => {
+        let queryString = `select * from users where user_email = "${user_email}"`;
+        result = await execute(queryString);
+        return result[0][0];
+    },
+    
+    get_user_by_nickname : async (user_nickname) => {
+        let queryString = `select * from users where user_nickname = "${user_nickname}"`;
+        result = await execute(queryString);
+        return result[0][0];
+    },
 
-        get_user_by_refreshToken : async (refreshToken) => {
-            let queryString = `select * from users where refreshToken="${refreshToken}"`;
-            result = await execute(queryString);
-            return result[0][0];
-        },
+    get_user_by_refreshToken : async (refreshToken) => {
+        let queryString = `select * from users where refreshToken="${refreshToken}"`;
+        result = await execute(queryString);
+        return result[0][0];
+    },
 
-        update_user_pw : (user_id, user_pw, salt) => {
-            let queryString = `update users set user_pw = "${user_pw}", salt = "${salt}" where id = "${user_id}"`
-            execute(queryString);
-		},
+    update_user_pw : (user_id, user_pw, salt) => {
+        let queryString = `update users set user_pw = "${user_pw}", salt = "${salt}" where id = "${user_id}"`
+        return execute(queryString);
+    },
 
-        update_user_point : (user_id, point) => {
-            let queryString = `update users set point = ${point} where id = "${user_id}"`
-            execute(queryString);
-        },
+    update_user_point : (user_id, point) => {
+        let queryString = `update users set point = ${point} where id = "${user_id}"`
+        return execute(queryString);
+    },
 
-        update_refreshToken : (user_email, refreshToken) => {
-            let queryString = `update users set refreshToken = "${refreshToken}" where user_email = "${user_email}"`;
-            execute(queryString);
-        },
-        
-        delete_user : (user_email) => {
-            let queryString = `delete from users where user_email = "${user_email}"` 
-            execute(queryString);
-        }
+    update_refreshToken : (user_email, refreshToken) => {
+        let queryString = `update users set refreshToken = "${refreshToken}" where user_email = "${user_email}"`;
+        return execute(queryString);
+    },
+    
+    delete_user : (user_email) => {
+        let queryString = `delete from users where user_email = "${user_email}"` 
+        return execute(queryString);
     }
 }
--- service/user_service.js
+++ service/user_service.js
@@ -1,4 +1,4 @@
-const Users = require('../models/users').user;
+const Users = require('../models/users');
 const mailer = require('../config/email_config');
 const { createHashedPassword } = require('../utils/hash');
 const { generate_tokens, verify_jwt } = require('../utils/jwt_service');
@@ -6,23 +6,22 @@ require("dotenv").config();
 
 module.exports = {
 	check_duplicated : async (req, res) => {
-		// 중복회원 검사
 		const user = await Users.get_user_by_email(req.body.user_email);
-		if (user)
-			return res.status(403).json({result : false, message : "이미 가입한 이메일입니다."});
-		else
-			return res.json({result : true, message : "회원가입이 가능한 이메일입니다."});
+
+		if (user) return res.status(403).json({result : false, message : "이미 가입한 이메일입니다."});
+		else      return res.json({result : true, message : "회원가입이 가능한 이메일입니다."});
 	},
 	
 	validate_email : (req, res) => {
-		let user_email = req.body.user_email;
-		let transporter = mailer.smtpTransport();
-		let maxNum = 999999;
-		let minNum = 111111;
+		const user_email = req.body.user_email;
+		const transporter = mailer.smtpTransport();
+		const maxNum = 999999;
+		const minNum = 111111;
 		
 		// minNum <= authNum <= maxNum
-		let authNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
-		let mailOptions = mailer.mailOptions(user_email, authNum);
+		const authNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+		const mailOptions = mailer.mailOptions(user_email, authNum);
 
 		transporter.sendMail(mailOptions, function (err, info) {
 			if (err) {
@@ -39,33 +38,43 @@ module.exports = {
 	validate_nickname : async (req, res) => {
 		const user = await Users.get_user_by_nickname(req.body.user_nickname);
 		if (user) return res.status(403).json({ message : "이미 사용중인 닉네임입니다." });
-		return res.json({ message : "사용 가능한 닉네임입니다." });
+		else      return res.json({ message : "사용 가능한 닉네임입니다." });
 	},
 	
 	register_user : async (req, res) => {
 		const user = Users.get_user_by_email(req.body.user_email);
+
 		if (req.body.user_pw.length < 8) 
 			return res.status(400).json({ message : "비밀번호는 8자 이상으로 입력해주세요"});
+
 		if (await user)
 			return res.status(403).json({ message: "이미 가입하셨습니다." });
+
 		const { hashedPassword, salt } = await createHashedPassword(req.body.user_pw);
+
 		await Users.insert_user(req.body.user_email, hashedPassword, req.body.user_nickname, salt);
-		return res.status(201).json({result: true, message: "회원가입에 성공하였습니다."})
+
+		return res.status(201).json({result: true, message: "회원가입에 성공하였습니다."});
 	},
 	
 	login : async (req, res) => {
 		const user = await Users.get_user_by_email(req.body.user_email);
 		const { hashedPassword } = await createHashedPassword(req.body.user_pw, user.salt);
+
 		if (user.user_pw === hashedPassword) {
 			const { accessToken, refreshToken } = generate_tokens(req.body.user_email); // token 발급
+
 			await Users.update_refreshToken(req.body.user_email, refreshToken);
+
 			res.cookie('accessToken', accessToken);
 			res.cookie('refreshToken', refreshToken);
+
 			return res.json({
 				result : true,
 				message : "로그인에 성공하였습니다."
 			});
-		};
+		}
+
 		return res.status(401).json({
 			result : false, 
 			message : "이메일 또는 비밀번호가 잘못되었습니다."
@@ -75,6 +84,7 @@ module.exports = {
 	get_info : async (req, res) => {
 		const user_email = verify_jwt(req.cookies.accessToken, 'access').email;
 		const user = await Users.get_user_by_email(user_email);
+
 		return res.json({
 			email : user.user_email,
 			nickname : user.user_nickname,
@@ -86,11 +96,13 @@ module.exports = {
 		const user_email = verify_jwt(req.cookies.accessToken, 'access').email;
 		const user = await Users.get_user_by_email(user_email);
 		const { hashedPassword } = await createHashedPassword(req.body.user_pw, user.salt);
+
 		if (hashedPassword ===  user.user_pw) {
 			return res.status(200).json({
 				message : "비밀번호 인증에 성공하였습니다."
 			});
 		};
+
 		return res.status(403).json({
 			message : "비밀번호 인증 실패"
 		});
@@ -100,7 +112,9 @@ module.exports = {
 		const user_email = verify_jwt(req.cookies.accessToken, 'access').email;
 		const user = await Users.get_user_by_email(user_email);
 		const {hashedPassword, salt} = await createHashedPassword(req.body.user_pw);
+
 		await Users.update_user_pw(user.id, hashedPassword, salt);
+
 		return res.json({
 			message : "비밀번호가 변경되었습니다."
 		});
@@ -122,7 +136,9 @@ module.exports = {
 		const user_email = verify_jwt(req.cookies.accessToken, 'access').email;
 		const user = await Users.get_user_by_email(user_email);
 		const point = user.point + req.body.add_point;
+
 		await Users.update_user_point(user.id, point);
+
 		return res.json({
 			message : "포인트가 변경되었습니다.",
 			point : point
